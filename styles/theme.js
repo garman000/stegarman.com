1632528f21d03e36c7100866af2b94b582fbab9c
garman000 <garman000@gmail.com>
2022-07-01 01:00:00 +0300

    first init

--- styles/theme.js
+++ styles/theme.js
@@ -2,8 +2,14 @@ import { extendTheme } from '@chakra-ui/react'
 import { theme as chakraTheme } from '@chakra-ui/react'
 import { createBreakPoints } from "@chakra-ui/theme-tools"
 
+const fonts = {
+    ...chakraTheme.fonts,
+}
 const overrides = {
     ...chakraTheme,
+    fonts,
 }
 
 const customTheme = extendTheme(overrides)
+
+export default customeTheme;
