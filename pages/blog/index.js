0e910007da171c8d5f4daebb4fbf4b173163727d
garman000 <garman000@gmail.com>
2022-07-02 11:18:57 +0300

    late changes to style

---
+++ pages/blog/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Box, Heading } from "@chakra-ui/react"
+import Blog from "../../components/blog"
+import ImageOne from "../../components/imageOne"
+import Image2 from "../../components/Image2"
+
+export default function blog() {
+    return (
+
+    <Box>
+         <Heading>Hello</Heading>
+    </Box>
+
+    )
+}
